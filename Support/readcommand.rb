07b41b00013ef4148a7cdecbef15fd4699efbdf5
Kenny Liou <kenny.liou@moovweb.com>
2011-07-18 10:13:56 -0700

    check if we are looking at lib.scss

--- Support/readcommand.rb
+++ Support/readcommand.rb
@@ -2,24 +2,29 @@
 
 require 'plist'
 
+
+bondule_location = ARGV[0]
 project_path = ARGV[1]
-support_path = ARGV[0]
+file_path = ARGV[2]
+file_name = File.basename("#{file_path}")
 
-begin
-  hash = []
+if file_name == "lib.scss"
+  begin
+    hash = []
+    #lib_file = File.open("#{project_path}/asset/stylesheet/lib.scss")
   
-  File.open("#{project_path}/assets/stylesheets/lib.scss") do |f|
-   f.grep( /\@mixin\s\w*\(\w*\)\s\{/ ) do |line|
-      line["@mixin "] = ""
-      line[" {"] = ""
-      line["()"] = ""
-      line = line.chop
-      hash += [:insert => "(${1:});", :display => "@include #{line}"]
+    File.open("#{project_path}/assets/stylesheets/lib.scss") do |f|
+     f.grep( /\@mixin\s\w*\(\w*\)\s\{/ ) do |line|
+        line["@mixin "] = ""
+        line[" {"] = ""
+        line["()"] = ""
+        line = line.chop
+        hash += [:insert => "(${1:});", :display => "@include #{line}"]
+      end
+      hash.save_plist("#{bondule_location}/test.plist")
     end
-    File.open("#{support_path}/mixins.plist", "w")
-    hash.save_plist("#{support_path}/mixins.plist")
-  end
-  puts "plist is generated at #{support_path}/mixins.plist"
-rescue
+    puts "plist is generated"
+  rescue
 
+  end
 end
