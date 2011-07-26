01dfaf46ec711c684925c34366c4183f6b30c0c6
Kenny Liou <kenny.liou@moovweb.com>
2011-07-26 10:49:26 -0700

    this should be able to capture all mixins

--- Support/readcommand.rb
+++ Support/readcommand.rb
@@ -8,20 +8,24 @@ project_path = ARGV[1]
 file_path = ARGV[2]
 file_name = File.basename("#{file_path}")
 
-if file_name == "lib.scss"
+puts "check file #{file_name} ..."
+puts "check file path #{file_path} ..."
+puts "check project path #{project_path} ..."
+puts "check bondule location #{bondule_location} ..."
+if file_name == "_lib.scss"
   begin
+    puts "generating mixins.plist ..."
     hash = []
-    #lib_file = File.open("#{project_path}/asset/stylesheet/lib.scss")
-  
-    File.open("#{project_path}/assets/stylesheets/lib.scss") do |f|
-     f.grep( /\@mixin\s\w*\(\w*\)\s\{/ ) do |line|
+    File.open("#{project_path}/assets/stylesheets/_lib.scss") do |f|
+     f.grep( /\@mixin\s\w*\([\$\w*\:?\#?\w*]*\)\s\{/ ) do |line|
+        puts line
         line["@mixin "] = ""
         line[" {"] = ""
         line["()"] = ""
         line = line.chop
         hash += [:insert => "(${1:});", :display => "@include #{line}"]
       end
-      hash.save_plist("#{bondule_location}/test.plist")
+      hash.save_plist("#{bondule_location}/mixins.plist")
     end
     puts "plist is generated"
   rescue
